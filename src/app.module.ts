ecaa3166761a74801ed2be5c50ddac0a2eac47ff
695954085 <695954085@qq.com>
2018-12-07 11:41:28 +0800

    feat: 导入模块，以及添加常量

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
+import { BusinessModule } from './business/business.module';
 
 @Module({
-  imports: [DatabaseModule, LoginModule],
+  imports: [DatabaseModule, AuthModule, LoginModule, BusinessModule],
 })
 export class AppModule {}
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,31 @@
 // database module provider token
 export const DATABASE_PROVIDER_TOKEN = 'SequlizeToken';
-// staff 模块 provider token
-export const STAFF_PROVIDER_TOKEN = 'StaffRepository';
 // crypto 密钥
 export const SECRET = 'order-system';
+
+// ================================= staff start
+// staff 模块 provider token
+export const STAFF_PROVIDER_TOKEN = 'StaffRepository';
 // 员工已存在
 export const STAFFALREADYEXIST = 'staff_already_exists';
 // 员工不存在
 export const STAFFNOEXIST = 'staff_no_exists';
+// ================================  staff end
+
+// ================================ login start
 // 成功注册
 export const SUCCESS_REGISTER = 'success_register';
 // 密码错误
 export const FAIL_PASSWORD_ERROR = 'fail_password_wrong';
 // 登录成功
 export const SUCCESS_LOGIN = 'success_login';
+// ================================ login end
+
+// ================================ vendor start
+// vendor 模块 provider token
+export const VENDOR_PROVIDER_TOKEN = 'VendorReposity';
+// vendor已存在
+export const VENDORALREADYEXIST = 'vendor_already_exists';
+// vendor添加成功
+export const VENDORINSERTSUCCESS = 'vendor_created_success';
+// ================================ vendor end
